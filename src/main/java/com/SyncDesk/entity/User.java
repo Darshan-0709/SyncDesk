a687ae556e20d3cafa4892c24551bddf9a9a8b1b
Darshan <104707176+Darshan-0709@users.noreply.github.com>
2024-10-17 08:22:17 +0530

    Update User.java

--- src/main/java/com/SyncDesk/entity/User.java
+++ src/main/java/com/SyncDesk/entity/User.java
@@ -13,17 +13,17 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    // @Column(nullable = false, length=100)
+     @Column(nullable = false, length=100)
     private String fullName;
-    // @Column(unique = true, nullable = false, length=255)
+     @Column(unique = true, nullable = false, length=255)
     private String email;
-    // @Column(nullable = false, length = 255)
+     @Column(nullable = false, length = 255)
     private String password;
-    // @Column(nullable = false, updatable = false)
+     @Column(nullable = false, updatable = false)
     private LocalDateTime createdAt;
-    // @Column(nullable = false)
+     @Column(nullable = false)
     private LocalDateTime updatedAt;
 
     @PrePersist
