910547484435185df656e706d5c01987bb517cf1
Darshan <104707176+Darshan-0709@users.noreply.github.com>
2024-10-17 08:22:01 +0530

    Create UserLoginDTO.java

---
+++ src/main/java/com/SyncDesk/dto/user/UserLoginDTO.java
@@ -0,0 +1,14 @@
+package com.SyncDesk.dto.user;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Getter;
+
+@Getter
+public class UserLoginDTO {
+    @NotEmpty(message = "Email is required")
+    @Email(message = "Invalid Email format")
+    private String email;
+    @NotEmpty(message = "Password is required")
+    private String password;
+}
