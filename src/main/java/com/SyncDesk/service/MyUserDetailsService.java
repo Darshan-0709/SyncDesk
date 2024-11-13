2e2f067034fe327b529732dcc957ca550e24a754
Darshan <104707176+Darshan-0709@users.noreply.github.com>
2024-11-13 10:06:58 +0530

    Create MyUserDetailsService.java

---
+++ src/main/java/com/SyncDesk/service/MyUserDetailsService.java
@@ -0,0 +1,23 @@
+package com.SyncDesk.service;
+
+import com.SyncDesk.entity.User;
+import com.SyncDesk.entity.UserPrincipal;
+import com.SyncDesk.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String name) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(name)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + name));
+        return new UserPrincipal(user);
+    }
+}
