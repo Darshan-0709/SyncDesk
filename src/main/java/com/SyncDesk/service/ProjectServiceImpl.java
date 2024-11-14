533ef87814508c9594b32f62f39f4a9b7d80d335
Darshan <104707176+Darshan-0709@users.noreply.github.com>
2024-11-14 10:07:41 +0530

    Update ProjectServiceImpl.java

--- src/main/java/com/SyncDesk/service/ProjectServiceImpl.java
+++ src/main/java/com/SyncDesk/service/ProjectServiceImpl.java
@@ -4,12 +4,20 @@ import com.SyncDesk.dto.project.CreateProjectDTO;
 import com.SyncDesk.dto.project.ProjectDTO;
 import com.SyncDesk.dto.project.UpdateProjectDTO;
 import com.SyncDesk.entity.Project;
+import com.SyncDesk.entity.ProjectMember;
+import com.SyncDesk.entity.Role;
 import com.SyncDesk.entity.User;
+import com.SyncDesk.repository.ProjectMemberRepository;
 import com.SyncDesk.repository.ProjectRepository;
+import com.SyncDesk.repository.RoleRepository;
 import com.SyncDesk.repository.UserRepository;
 import com.SyncDesk.utils.DTOConverter;
+import jakarta.transaction.Transactional;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import static com.SyncDesk.utils.DTOConverter.convertToProjectDTO;
@@ -19,17 +27,23 @@ public class ProjectServiceImpl implements ProjectService{
 
     private final ProjectRepository projectRepository;
     private final UserRepository userRepository;
+    private final ProjectMemberRepository projectMemberRepository;
+    private final RoleRepository roleRepository;
 
-    public ProjectServiceImpl(ProjectRepository projectRepository, UserRepository userRepository) {
+    public ProjectServiceImpl(ProjectRepository projectRepository, UserRepository userRepository, ProjectRepository projectMemberRepository, ProjectMemberRepository projectMemberRepository1, RoleRepository roleRepository) {
         this.projectRepository = projectRepository;
         this.userRepository = userRepository;
+        this.projectMemberRepository = projectMemberRepository1;
+        this.roleRepository = roleRepository;
     }
 
     @Override
+    @Transactional
     public ProjectDTO createProject(CreateProjectDTO createProjectDTO) {
         if(projectRepository.existsByName(createProjectDTO.getName())){
             throw new ProjectAlreadyExists("Project already Exists");
         }
+
         Project project = new Project();
         project.setName(createProjectDTO.getName());
         if(createProjectDTO.getDescription() != null && !createProjectDTO.getDescription().isEmpty()){
@@ -39,13 +53,32 @@ public class ProjectServiceImpl implements ProjectService{
         if(createProjectDTO.getEndDate() != null){
             project.setEndDate(createProjectDTO.getEndDate());
         }
-        User user = userRepository
-                .findById(1L)
+
+        String currentUserEmail = null;
+        Object userPrincipal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if(userPrincipal instanceof UserDetails){
+            currentUserEmail = ((UserDetails) userPrincipal).getUsername();
+        }else{
+            currentUserEmail = userPrincipal.toString();
+        }
+
+        User currentUser = userRepository
+                .findByEmail(currentUserEmail)
                 .orElseThrow(() -> new NoSuchUserFoundException("Unauthorized access"));
 
-        project.setUser(user);
-
+        project.setUser(currentUser);
         project = projectRepository.save(project);
+
+        Role adminRole = roleRepository.findByName("ADMIN")
+                .orElseThrow(() -> new RuntimeException("ADMIN role not found"));
+
+        ProjectMember projectMember = new ProjectMember();
+        projectMember.setUser(currentUser);
+        projectMember.setProject(project);
+        projectMember.setRole(adminRole);
+        projectMember.setJoinedAt(LocalDate.now());
+
+        projectMemberRepository.save(projectMember);
 
         return convertToProjectDTO(project);
     }
