7a86ef3c4d612a2b4f2ca7386e3b6f6ca589fc0c
Darshan <104707176+Darshan-0709@users.noreply.github.com>
2024-11-15 22:35:08 +0530

    Update ProjectRepository.java

--- src/main/java/com/SyncDesk/repository/ProjectRepository.java
+++ src/main/java/com/SyncDesk/repository/ProjectRepository.java
@@ -1,8 +1,11 @@
 package com.SyncDesk.repository;
 
 import com.SyncDesk.entity.Project;
+import com.SyncDesk.entity.ProjectMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface ProjectRepository extends JpaRepository<Project, Long> {
