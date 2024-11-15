137e9bc7459e0598e7a226ebcc18d24c3d4102ab
Darshan <104707176+Darshan-0709@users.noreply.github.com>
2024-11-15 22:34:54 +0530

    Update ProjectMemberController.java

--- src/main/java/com/SyncDesk/controller/ProjectMemberController.java
+++ src/main/java/com/SyncDesk/controller/ProjectMemberController.java
@@ -1,14 +1,18 @@
 package com.SyncDesk.controller;
 
+import com.SyncDesk.common.ApiResponse;
 import com.SyncDesk.dto.project.ProjectDTO;
+import com.SyncDesk.dto.project_member.AddMemberDTO;
+import com.SyncDesk.dto.project_member.ChangeRoleDTO;
 import com.SyncDesk.dto.project_member.ProjectMemberDTO;
 import com.SyncDesk.service.ProjectMemberService;
+import com.SyncDesk.service.ProjectMemberServiceImpl;
+import com.SyncDesk.utils.NoProjectFoundException;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,16 +20,45 @@ import java.util.List;
 @RequestMapping("/members")
 public class ProjectMemberController {
 
-    public final ProjectMemberService projectMemberService;
+    public final ProjectMemberServiceImpl projectMemberService;
 
-    public ProjectMemberController(ProjectMemberService projectMemberService) {
+    public ProjectMemberController(ProjectMemberServiceImpl projectMemberService) {
         this.projectMemberService = projectMemberService;
     }
 
 
     @GetMapping("/{id}")
-    public ResponseEntity<?> getAllMembers(@PathVariable Long id){
-        List<ProjectMemberDTO> projectMembers =projectMemberService.getAllMembers(id);
-        return new ResponseEntity<>(projectMembers, HttpStatus.OK);
+    public ResponseEntity<ApiResponse<List<ProjectMemberDTO>>> getAllMembers(@PathVariable Long id) {
+        try {
+            List<ProjectMemberDTO> projectMembersDTO = projectMemberService.getAllByProjectId(id);
+            return ResponseEntity.ok(new ApiResponse<>("Members fetched successfully", projectMembersDTO));
+        } catch (NoProjectFoundException | Exception e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body(new ApiResponse<>(e.getMessage(), null));
+        }
     }
+
+    @PostMapping("/{id}")
+    public ResponseEntity<ApiResponse<ProjectMemberDTO>> addMember(@PathVariable Long id, @Valid @RequestBody AddMemberDTO addMemberDTO){
+        try {
+            ProjectMemberDTO projectMembersDTO = projectMemberService.addMember(id, addMemberDTO);
+            return ResponseEntity.ok(new ApiResponse<>("New member added successfully",projectMembersDTO));
+        } catch (Exception e) {
+            
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body(new ApiResponse<>(e.getMessage(), null));
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ApiResponse<ProjectMemberDTO>> editMember(@PathVariable Long id, @RequestParam String newRole) {
+        try {
+            ProjectMemberDTO projectMemberDTO = projectMemberService.editMember(id, newRole);
+            return ResponseEntity.ok(new ApiResponse<>("Role updated successfully", projectMemberDTO));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body(new ApiResponse<>(e.getMessage(), null));
+        }
+    }
+
 }
