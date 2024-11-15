ef300c99ea1b0d033b32382c447dc859fa82f673
Darshan <104707176+Darshan-0709@users.noreply.github.com>
2024-11-15 22:35:06 +0530

    Update ProjectMemberRepository.java

--- src/main/java/com/SyncDesk/repository/ProjectMemberRepository.java
+++ src/main/java/com/SyncDesk/repository/ProjectMemberRepository.java
@@ -1,9 +1,19 @@
 package com.SyncDesk.repository;
 
+import com.SyncDesk.entity.Project;
 import com.SyncDesk.entity.ProjectMember;
+import com.SyncDesk.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface ProjectMemberRepository extends JpaRepository<ProjectMember, Long> {
     // Define any additional query methods if necessary
+    List<ProjectMember> findAllByProjectId(Long id);
+
+    Optional<ProjectMember> findByUserId(Long id);
+
+    boolean existsByProjectAndUser(Project project, User memberToAdd);
 }
 
