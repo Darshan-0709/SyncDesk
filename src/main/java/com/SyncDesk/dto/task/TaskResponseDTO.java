6a8e81bbf5e58d0b1a3d60171f2dde5560c9c941
Darshan <104707176+Darshan-0709@users.noreply.github.com>
2024-11-19 14:21:34 +0530

    Create TaskResponseDTO.java

---
+++ src/main/java/com/SyncDesk/dto/task/TaskResponseDTO.java
@@ -0,0 +1,26 @@
+package com.SyncDesk.dto.task;
+
+import com.SyncDesk.dto.user.UserDTO;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Builder
+public class TaskResponseDTO {
+
+    private Long id;
+    private String name;
+    private String description;
+    private UserDTO createdBy;
+    private UserDTO assignedTo;
+    private String priority;
+    private String status;
+    private LocalDate dueDate;
+}
