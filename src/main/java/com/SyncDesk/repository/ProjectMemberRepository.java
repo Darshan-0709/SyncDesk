c02c64b2b88354390c43dc10d2d8f140bcd3a8b6
Darshan <104707176+Darshan-0709@users.noreply.github.com>
2024-12-17 22:02:44 +0530

    Update ProjectMemberRepository.java

--- src/main/java/com/SyncDesk/repository/ProjectMemberRepository.java
+++ src/main/java/com/SyncDesk/repository/ProjectMemberRepository.java
@@ -2,6 +2,8 @@ package com.SyncDesk.repository;
 
 import com.SyncDesk.entity.ProjectMember;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,7 +15,11 @@ public interface ProjectMemberRepository extends JpaRepository<ProjectMember, Lo
     Optional<ProjectMember> findByUserId(Long id);
 
     boolean existsByProjectIdAndUserId(Long project, Long memberToAdd);
-
     Optional<ProjectMember> findByUserIdAndProjectId(Long id, Long projectId);
+    @Query("SELECT pm FROM ProjectMember pm " +
+            "JOIN FETCH pm.project p " +
+            "JOIN FETCH pm.role r " +
+            "WHERE pm.user.id = :userId")
+    List<ProjectMember> findAllByUserIdWithProjectsAndRoles(@Param("userId") Long userId);
 }
 
