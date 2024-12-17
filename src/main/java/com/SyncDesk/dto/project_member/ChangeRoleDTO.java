2047dde41fbf498c883be9a3b5d84907d9dfd8c2
Darshan <104707176+Darshan-0709@users.noreply.github.com>
2024-12-17 22:02:29 +0530

    Update ChangeRoleDTO.java

--- src/main/java/com/SyncDesk/dto/project_member/ChangeRoleDTO.java
+++ src/main/java/com/SyncDesk/dto/project_member/ChangeRoleDTO.java
@@ -1,10 +1,13 @@
 package com.SyncDesk.dto.project_member;
 
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 @Getter
 public class ChangeRoleDTO {
+    @NotNull(message = "Id is required")
+    private Long id;
     @NotEmpty(message = "Role is required")
     private String role;
 }
