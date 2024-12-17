e482460ed939054612b6995189755971a78a4a35
Darshan <104707176+Darshan-0709@users.noreply.github.com>
2024-12-17 22:02:56 +0530

    Update ProjectService.java

--- src/main/java/com/SyncDesk/service/ProjectService.java
+++ src/main/java/com/SyncDesk/service/ProjectService.java
@@ -5,12 +5,13 @@ import com.SyncDesk.dto.project.ProjectDTO;
 import com.SyncDesk.dto.project.UpdateProjectDTO;
 import com.SyncDesk.utils.NoProjectFoundException;
 import com.SyncDesk.utils.ProjectAlreadyExistsException;
+import com.SyncDesk.utils.ResourceNotFoundException;
 
 import java.util.List;
 
 public interface ProjectService {
     ProjectDTO createProject(CreateProjectDTO createProjectDTO) throws ProjectAlreadyExistsException;
-    ProjectDTO updateProject(Long id, UpdateProjectDTO updateProjectDTO) throws NoProjectFoundException;
+    ProjectDTO updateProject(Long id, UpdateProjectDTO updateProjectDTO) throws NoProjectFoundException, ResourceNotFoundException;
     boolean deleteProject(Long id);
     List<ProjectDTO> getAllProject();
     ProjectDTO getById(Long id) throws NoProjectFoundException;
