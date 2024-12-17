dfaf5830748674dd39ec9a0286ad441d3d88c453
Darshan <104707176+Darshan-0709@users.noreply.github.com>
2024-12-17 22:02:24 +0530

    Update ProjectDTO.java

--- src/main/java/com/SyncDesk/dto/project/ProjectDTO.java
+++ src/main/java/com/SyncDesk/dto/project/ProjectDTO.java
@@ -1,18 +1,23 @@
 package com.SyncDesk.dto.project;
 
+import com.SyncDesk.dto.project_member.ProjectMemberDTO;
 import com.SyncDesk.dto.user.UserDTO;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Getter
 @Setter
 public class ProjectDTO {
-    public Long id;
-    public String name;
-    public UserDTO admin;
-    public String description;
-    public LocalDate startDate;
-    public LocalDate endDate;
+    private Long id;
+    private String name;
+    private UserDTO admin;
+    private String userRole;
+    private String description;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private List<ProjectMemberDTO> members;
 }
+
